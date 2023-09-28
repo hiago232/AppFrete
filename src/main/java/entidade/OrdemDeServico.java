d2515f3b939a363b577b599b798384b74f039268
Hiago Ramos <hiago232@hotmail.com>
2023-09-28 19:25:03 -0300

    Primeiro commit

---
+++ src/main/java/entidade/OrdemDeServico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author Usuário
+ */
+public class OrdemDeServico {
+    
+    public static void menu(){
+        String itens = "";
+        String add = "";
+        String menuos = "1 - Add item      2 - Localização\n"
+                +"3 - Destino      ";
+    }
+    
+}
