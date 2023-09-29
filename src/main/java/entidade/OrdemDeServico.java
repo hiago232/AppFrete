b9835b92a0bb6da222607236d0a3b004eda296e8
Hiago Ramos <hiago232@hotmail.com>
2023-09-29 18:31:32 -0300

    metodo addItem

--- src/main/java/entidade/OrdemDeServico.java
+++ src/main/java/entidade/OrdemDeServico.java
@@ -4,19 +4,22 @@
  */
 package entidade;
 import javax.swing.*;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author Usuário
  */
 public class OrdemDeServico {
+    public String listaItens ="";
     public String moveis = "";
     public String local = "";
     public String destino = "";
     public double distancia = 0 ;
     public String tempo = "";
     public double valortotal = 0;
-    
+    public String t = "Ordem de Serviço";
     // Construtor padrão
     public OrdemDeServico(){};
     
@@ -24,23 +27,20 @@ public class OrdemDeServico {
     public void menu(){
      
         // Declaração de variáveis
-        
-        
+        List<String> itens = new ArrayList<>();
         boolean sair = true;
         int op = 0;
-                
-        String t = "Ordem de Serviço";
-        String itens = "";
-        String add = "";
+    
         while (sair){
             String menuos = "1 - Add item\n"
-                +"2 - Local inicial:   "+local+"\n"
-                +"3 - Destino:    "+destino+"\n"
-                +"4 - Distância:   "+distancia+" KM\n"
-                +"5 - Combustivel \n"
-                +"6 - Tempo \n"
-                +"7 - Valor total \n"
-                +"8 - Fechar OS\n";
+                +"2 - Exibir lista\n"
+                +"3 - Local inicial:   "+local+"\n"
+                +"4 - Destino:    "+destino+"\n"
+                +"5 - Distância:   "+distancia+" KM\n"
+                +"6 - Combustivel \n"
+                +"7 - Tempo \n"
+                +"8 - Valor total \n"
+                +"9 - Fechar OS\n";
         
             
             op = Integer.parseInt(JOptionPane.
@@ -51,22 +51,28 @@ public class OrdemDeServico {
             
             switch (op) {
                 case 1 : 
-                    //addItem();
+                    itens = addItem(itens);
                     break;
-                case 2 : 
+                case 2 :
+                    for (String item : itens) {
+                        listaItens = listaItens + "\n" + item;
+                    }
+                    JOptionPane.showMessageDialog(null, listaItens);
+                    break ;
+                case 3 : 
                     System.out.println(); 
                     local = JOptionPane.showInputDialog(null,
                             "Insira o local inicial: ",
                             t,
                             JOptionPane.QUESTION_MESSAGE);
                     break;
-                case 3 :                    
+                case 4:                    
                     destino = JOptionPane.showInputDialog(null,
                             "Insira o destino final: ",
                             t,
                             JOptionPane.QUESTION_MESSAGE);
                     break;
-                case 4 :
+                case 5 :
                     
                     distancia = Double.parseDouble(JOptionPane.
                             showInputDialog(null,
@@ -74,16 +80,17 @@ public class OrdemDeServico {
                             t,
                             JOptionPane.QUESTION_MESSAGE));
                     break;
-                case 5 : 
+                case 6 : 
+                    
                     //combustivel();
                     break;
-                case 6 :
+                case 7 :
                     //Tempo();
                     break;
-                case 7 :
+                case 8 :
                     //valorTotal();
                     break;
-                case 8 :
+                case 9 :
                     sair = false;
                     break;
                 default :
@@ -95,6 +102,21 @@ public class OrdemDeServico {
         
 
         
+    }
+    public List<String> addItem (List<String> itens){
+        boolean voltar = true;
+        String item = "";
+        while (voltar){
+            
+            item = JOptionPane.showInputDialog(null,
+                    "Insira o item abaixo: ", t,1);
+            if ("voltar".equals(item)){
+                return itens;
+            }
+            itens.add(item);
+        }
+        return itens;
+    
     }
     
 }
